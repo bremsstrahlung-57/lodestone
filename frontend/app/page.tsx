5d0ebc42fc0178de9dabf562119212342333675b
bremsstrahlung-57 <sagar292905@gmail.com>
2026-03-02 17:38:03 +0530

    Remove Next.js frontend

--- frontend/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default async function Home() {
-    const res = await fetch("http://127.0.0.1:8000/health", {
-        cache: "no-store",
-    });
-
-    const data = await res.json();
-
-    return <pre>{JSON.stringify(data, null, 2)}</pre>;
-}
